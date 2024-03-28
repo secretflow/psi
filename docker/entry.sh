164962a5601ea47f7dee5712a06e945e50c13e3f
Jun Feng <fengjun.feng@antgroup.com>
2024-03-28 16:21:09 +0800

    add arm docker build

--- docker/entry.sh
+++ docker/entry.sh
@@ -10,4 +10,5 @@ cd src_copied
 
 bazel build psi:main -c opt --config=linux-release --repository_cache=/tmp/bazel_repo_cache
 chmod 777 bazel-bin/psi/main
-cp bazel-bin/psi/main ../src/docker/
+mkdir -p ../src/docker/linux/amd64
+cp bazel-bin/psi/main ../src/docker/linux/amd64

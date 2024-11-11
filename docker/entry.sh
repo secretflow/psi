f9ce44a1ab735f3eb5fc06a56751effc2b150a88
huocun <dongwenjun.dwj@antgroup.com>
2024-11-11 17:27:42 +0800

    repo-sync-2024-11-11T17:27:35+0800

--- docker/entry.sh
+++ docker/entry.sh
@@ -5,6 +5,8 @@ set -ex
 cp -r src src_copied
 cd src_copied
 
+conda install -y perl=5.20.3.1
+
 bazel build psi:main -c opt --config=linux-release --repository_cache=/tmp/bazel_repo_cache
 chmod 777 bazel-bin/psi/main
 mkdir -p ../src/docker/linux/amd64

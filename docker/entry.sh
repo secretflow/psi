5efe4d9564995c8d5b488fdc1865be3975ca9505
yuyongqiang.yyq <yuyongqiang.yyq@antgroup.com>
2025-08-20 01:59:49 +0000

    fix docker image building and change bazel registry

--- docker/entry.sh
+++ docker/entry.sh
@@ -6,11 +6,11 @@ cp -r src src_copied
 cd src_copied
 
 conda install -y perl=5.20.3.1
+conda update -y sysroot_linux-64
 
 
 
-
-bazel build psi:main -c opt --config=linux-release --repository_cache=/tmp/bazel_repo_cache --remote_timeout=300s --remote_retries=10
+bazel build psi/apps/psi_launcher:main -c opt --config=linux-release --repository_cache=/tmp/bazel_repo_cache --remote_timeout=300s --remote_retries=10
 chmod 777 bazel-bin/psi/apps/psi_launcher/main
 mkdir -p ../src/docker/linux/amd64
 cp bazel-bin/psi/apps/psi_launcher/main ../src/docker/linux/amd64
